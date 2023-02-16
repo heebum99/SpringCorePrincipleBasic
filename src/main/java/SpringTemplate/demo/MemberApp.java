ebd9ae4fa741353a54838e7b84287a689cda45a8
heebum99 <heebum9955@naver.com>
2023-02-17 00:48:22 +0900

    회원 도메인 실행 및 테스트

---
+++ src/main/java/SpringTemplate/demo/MemberApp.java
@@ -0,0 +1,18 @@
+package SpringTemplate.demo;
+
+import SpringTemplate.demo.member.Grade;
+import SpringTemplate.demo.member.Member;
+import SpringTemplate.demo.member.MemberService;
+import SpringTemplate.demo.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
