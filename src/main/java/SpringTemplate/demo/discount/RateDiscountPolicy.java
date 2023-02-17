702dcb4e5169f8bdbd65e57429920f5ce27ed07a
heebum99 <heebum9955@naver.com>
2023-02-17 20:41:28 +0900

    새로운 할인 정책 개발

---
+++ src/main/java/SpringTemplate/demo/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package SpringTemplate.demo.discount;
+
+import SpringTemplate.demo.member.Grade;
+import SpringTemplate.demo.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
