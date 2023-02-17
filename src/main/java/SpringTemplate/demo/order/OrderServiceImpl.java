fa515f059d0defb93e56018a784100771ea5a74b
heebum99 <heebum9955@naver.com>
2023-02-17 22:47:49 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/SpringTemplate/demo/order/OrderServiceImpl.java
+++ src/main/java/SpringTemplate/demo/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package SpringTemplate.demo.order;
 
 import SpringTemplate.demo.discount.DiscountPolicy;
 import SpringTemplate.demo.discount.FixDiscountPolicy;
+import SpringTemplate.demo.discount.RateDiscountPolicy;
 import SpringTemplate.demo.member.Member;
 import SpringTemplate.demo.member.MemberRepository;
 import SpringTemplate.demo.member.MemoryMemberRepository;
@@ -9,7 +10,13 @@ import SpringTemplate.demo.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //할인 정책을 바꿈으로 코드를 변경하는것 => OCP위반.
+    /**
+     * private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+     * => private final DiscountPolicy로 바뀐다는 것은
+     * OrderServiceImpl는 인터페이스 DiscountPolicy 뿐만아니고 구현체 Fix ~ , Rate ~ 에도 영향을 받고 있으므로 DIP 위반.
+     **/
+    private DiscountPolicy discountPolicy; //인터페이스에만 의존 => DIP 적합, 현재는 NullPointException 발생
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
