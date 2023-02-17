c2cedb753b5fbefe1a1c5c3db25a658643a34692
heebum99 <heebum9955@naver.com>
2023-02-18 00:31:27 +0900

    Config 리팩터링

--- src/main/java/SpringTemplate/demo/AppConfig.java
+++ src/main/java/SpringTemplate/demo/AppConfig.java
@@ -2,6 +2,7 @@ package SpringTemplate.demo;
 
 import SpringTemplate.demo.discount.DiscountPolicy;
 import SpringTemplate.demo.discount.FixDiscountPolicy;
+import SpringTemplate.demo.member.MemberRepository;
 import SpringTemplate.demo.member.MemberService;
 import SpringTemplate.demo.member.MemberServiceImpl;
 import SpringTemplate.demo.member.MemoryMemberRepository;
@@ -13,10 +14,18 @@ public class AppConfig { //객체 생성과 연결을 담당
     //생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해 주입(연결)한다.
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
