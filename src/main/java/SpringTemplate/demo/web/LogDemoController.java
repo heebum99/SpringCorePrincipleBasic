9355ee163a212680aca6592f9c59eeef7cca3514
heebum99 <heebum9955@naver.com>
2023-02-27 00:30:26 +0900

    스코프와 Provider

--- src/main/java/SpringTemplate/demo/web/LogDemoController.java
+++ src/main/java/SpringTemplate/demo/web/LogDemoController.java
@@ -2,6 +2,7 @@ package SpringTemplate.demo.web;
 
 import SpringTemplate.demo.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +15,21 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    //MyLogger를 찾아서 연결해주는 DI가 아닌 MyLogger를 찾아주는 DL을 해준다.
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final MyLogger myLogger;
     //의존 관계 주입할때 오류발생 => MyLogger의 스코프는 request 타입이라 요청이 들어올때 빈이 생성이 되는데 아무런 요청이 없기때문에 오류 발생
 
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString(); //고객이 어떤 url을 요청했는지 알 수 있다.
+        MyLogger myLogger = myLoggerProvider.getObject(); //DL을 통해 MyLogger를 찾아온다.
         myLogger.setRequestURL(requestURL); //setter로 requestURL 값 초기화
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
