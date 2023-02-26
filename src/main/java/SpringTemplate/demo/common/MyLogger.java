50a4c3cddeb719765d5f7461c67eb63dd3f7994b
heebum99 <heebum9955@naver.com>
2023-02-27 01:17:20 +0900

    스코프와 프록시

--- src/main/java/SpringTemplate/demo/common/MyLogger.java
+++ src/main/java/SpringTemplate/demo/common/MyLogger.java
@@ -1,6 +1,7 @@
 package SpringTemplate.demo.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,9 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request") //HTTP 요청 하나당 하나씩 생성되고 요청이 끝나는 시점에 소멸
+//@Scope(value = "request") //HTTP 요청 하나당 하나씩 생성되고 요청이 끝나는 시점에 소멸
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) //이 클래스에 대해서 프록시(가짜)를 생성
+//이렇게하면 Provider대신 가짜 프록시 클래스를 만들어서 request와 관계없이 가짜 프록시 클래스를 다른 빈에 미리 주입해둘 수 있다.
 public class MyLogger {
 
     private String uuid;
--- src/main/java/SpringTemplate/demo/web/LogDemoController.java
+++ src/main/java/SpringTemplate/demo/web/LogDemoController.java
@@ -16,8 +16,8 @@ public class LogDemoController {
 
     private final LogDemoService logDemoService;
     //MyLogger를 찾아서 연결해주는 DI가 아닌 MyLogger를 찾아주는 DL을 해준다.
-    private final ObjectProvider<MyLogger> myLoggerProvider;
-//    private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     //의존 관계 주입할때 오류발생 => MyLogger의 스코프는 request 타입이라 요청이 들어올때 빈이 생성이 되는데 아무런 요청이 없기때문에 오류 발생
 
 
@@ -25,7 +25,8 @@ public class LogDemoController {
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString(); //고객이 어떤 url을 요청했는지 알 수 있다.
-        MyLogger myLogger = myLoggerProvider.getObject(); //DL을 통해 MyLogger를 찾아온다.
+//        MyLogger myLogger = myLoggerProvider.getObject(); //DL을 통해 MyLogger를 찾아온다.
+        System.out.println("myLogger = " + myLogger.getClass()); //출력시 가짜 프록시 클래스가 출력되는걸 알 수 있다.
         myLogger.setRequestURL(requestURL); //setter로 requestURL 값 초기화
 
         myLogger.log("controller test");
